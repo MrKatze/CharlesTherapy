a62dcc0afdd863ed59afd8ef1f839b0aa4a8d332
Ernesto2021Me <carlosmarcial9ahuajintepec@gmail.com>
2025-05-20 19:49:23 -0600

    Version 1 de Interfaces
    Implementacion de primeras versiones de Interfaces

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PrincipalComponent } from './componentes/principal/principal.component';
+import { HistorialComponent } from './componentes/historial/historial.component';
 
 export const routes: Routes = [
-    { path: '', component: PrincipalComponent },
+  { path: '', component: PrincipalComponent },
+  { path: 'historial', component: HistorialComponent }
 ];
