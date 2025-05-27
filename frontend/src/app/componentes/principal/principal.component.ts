05db4eda9a75c99621659d56f3d5a8625835328f
Jose Ramon Aragon Toledo <hudesde@gmail.com>
2025-05-26 18:26:46 -0600

    SideBarIndependiente

--- frontend/src/app/componentes/principal/principal.component.ts
+++ frontend/src/app/componentes/principal/principal.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import OpenAI from 'openai';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-principal',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, SidebarComponent],
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
